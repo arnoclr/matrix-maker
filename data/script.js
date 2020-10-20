f218965db94569e4a09c4f35b8a4d2fc4fa94735
Aryqs-Ipsum <45895096+Aryqs-Ipsum@users.noreply.github.com>
2020-10-20 12:01:13 +0200

    UI fix

--- data/script.js
+++ data/script.js
@@ -21,7 +21,7 @@ new Vue({
         ctx: '',
         previewCtx: '',
         overlayImage: '',
-        dests: '',
+        dests: [],
         addDestCode: '',
         current: {
             front: {},
@@ -29,24 +29,26 @@ new Vue({
             side: {},
         },
         fonts: {},
-        selectedFont: '',
         fontList,
-        frontText: '',
     },
     methods: {
         // Matrix
-        fillBitmapTextDraw: function(ctx, text, x, y, font,fillStyle) {
-            var curX = x;
-            for (var i = 0; i < text.length; i++) {
-                var char = text.substr(i, 1);
-                var cData = [];
+        fillBitmapTextDraw: function(ctx, text, x, y, font, fillStyle) {
+            if (localStorage[font]) {
+                var curX = x;
+                for (var i = 0; i < text.length; i++) {
+                    var char = text.substr(i, 1);
+                    var cData = [];
                     if (this.fonts[font][font][char] != null) {
                         cData = this.fonts[font][font][char];
                     } else {
                         this.$toast("Character '" + char + "' Not Found in font: '" + font + "'!");
                     }
-                this.bitmapDrawCharacter(ctx, curX, y, cData, fillStyle);
-                curX = curX + cData.glyph[0].length + this.fonts[font][font].charSpacing;
+                    this.bitmapDrawCharacter(ctx, curX, y, cData, fillStyle);
+                    curX = curX + cData.glyph[0].length + this.fonts[font][font].charSpacing;
+                }
+            } else {
+                this.$toast(font + ': is not usable');
             }
         },
         bitmapDrawCharacter: function(ctx, curX, y, cData, fillStyle) {
@@ -71,6 +73,11 @@ new Vue({
                 for (var i = 0; i < text.length; i++) {
                     var char = text.substr(i, 1);
                     var cData = [];
+                    // load font
+                    console.log(this.fonts[font])
+                    if (!this.fonts[font]) {
+                        this.loadFont(font);
+                    }
                     // if (char !== " ") {
                     if (this.fonts[font][font][char] != null) {
                         cData = this.fonts[font][font][char];
@@ -93,23 +100,76 @@ new Vue({
                 };
             }
         },
-        // write
+
+        // front
         writeFrontText: function() {
             this.ctx.fillStyle = "#000000";
-            this.ctx.fillRect(0, 0, 230, 32);
-            var dims = this.bitmapTextDims(document.getElementById("frontText").value, this.selectedFont);
-            this.fillBitmapTextDraw(this.ctx, document.getElementById("frontText").value, Math.round(115-(dims.width/2)), Math.round(16+(dims.height/2)), this.selectedFont, "#FF5500", ()=>{});
+            var tlx = (this.current.front.line) ? 50 : 0;
+            var textWidth = (this.current.front.line) ? 140 : 115;
+            this.ctx.fillRect(tlx, 0, 230, 32);
+            // 1/2 lines
+            if (this.current.front.text.includes('\n')) {
+                var splitedText = this.current.front.text.split('\n');
+                var dims = this.bitmapTextDims(splitedText[0], this.current.front.font);
+                this.fillBitmapTextDraw(this.ctx, splitedText[0], Math.round(textWidth-(dims.width/2)), Math.round(8+(dims.height/2)), this.current.front.font, this.current.front.color, ()=>{});
+                var dims = this.bitmapTextDims(splitedText[1], this.current.front.font);
+                this.fillBitmapTextDraw(this.ctx, splitedText[1], Math.round(textWidth-(dims.width/2)), Math.round(24+(dims.height/2)), this.current.front.font, this.current.front.color, ()=>{});
+            } else {
+                var dims = this.bitmapTextDims(this.current.front.text, this.current.front.font);
+                this.fillBitmapTextDraw(this.ctx, this.current.front.text, Math.round(textWidth-(dims.width/2)), Math.round(16+(dims.height/2)), this.current.front.font, this.current.front.color, ()=>{});
+            }
             this.renderCanvas(this.ctx, this.previewCtx);
         },
+
+        // side
+        writeSideText: function() {
+            this.ctx.fillStyle = "#000000";
+            this.ctx.fillRect(60, 32, 170, 32);
+            var dims = this.bitmapTextDims(this.current.side.text, this.current.side.font);
+            this.fillBitmapTextDraw(this.ctx, this.current.side.text, Math.round(145-(dims.width/2)), Math.round(48+(dims.height/2)), this.current.side.font, this.current.front.color, ()=>{});
+            this.renderCanvas(this.ctx, this.previewCtx);
+        },
+
+        // line
+        writeLineText: function() {
+            // background
+            this.ctx.fillStyle = this.current.line.back;
+            this.ctx.fillRect(0, 0, 50, 64);
+            var dims = this.bitmapTextDims(this.current.line.text, this.current.line.font);
+            // outline
+            var pxpy = [[24, 16], [25, 15], [26, 16], [25, 17], [24, 48], [25, 47], [26, 48], [25, 49]]
+            for (array of pxpy) {
+                this.fillBitmapTextDraw(this.ctx, this.current.line.text, Math.round(array[0]-(dims.width/2)), Math.round(array[1]+(dims.height/2)), this.current.line.font, this.current.line.outl, ()=>{});
+            }
+            // text
+            this.fillBitmapTextDraw(this.ctx, this.current.line.text, Math.round(25-(dims.width/2)), Math.round(16+(dims.height/2)), this.current.line.font, this.current.line.fore, ()=>{});
+            this.fillBitmapTextDraw(this.ctx, this.current.line.text, Math.round(25-(dims.width/2)), Math.round(48+(dims.height/2)), this.current.line.font, this.current.line.fore, ()=>{});
+        },
+
         // refresh matrix
         refreshMatrix: function() {
             this.writeFrontText();
+            this.writeSideText();
+            this.writeLineText();
+            this.renderCanvas(this.ctx, this.previewCtx);
+            this.saveCurrentIntoDests();
         },
 
         // utils
         renderCanvas: function(ctx, previewCtx) {
-            previewCtx.drawImage(canvas, 0, 0, 1024, 256);
+            previewCtx.drawImage(this.canvas, 0, 0, 1024, 256);
             previewCtx.drawImage(overlayImage, 0, 0, 1024, 256);
+        },
+        downloadCanvas: function() {
+            let downloadLink = document.createElement('a');
+            var imgName = `kpp.genav.ch.${this.current.code}.${this.current.name}.png`;
+            downloadLink.setAttribute('download', imgName);
+            let canvas = document.querySelector('canvas');
+            let dataURL = canvas.toDataURL('image/png');
+            let url = dataURL.replace(/^data:image\/png/,'data:application/octet-stream');
+            downloadLink.setAttribute('href', url);
+            downloadLink.click();
+            this.$toast(`${imgName} downloaded`);
         },
     
         drawPX: function(previewCtx, color, x, y) {
@@ -141,36 +201,67 @@ new Vue({
             } : null;
         },
         
+        // dest logic
         addDest: function(code) {
-            dest = {code: code, front: {}};
-            this.$set(this.dests, code, dest)
-            this.selectCurrent(code);
+            this.current = {
+                code: code,
+                name: '',
+                front: {
+                    font: '',
+                    text: 'FRONT',
+                    line: true,
+                    color: '#FF6A00',
+                },
+                line: {
+                    font: '',
+                    text: code,
+                    back: '#FF0000',
+                    fore: '#FFFFFF',
+                    outl: '#000000',
+                },
+                side: {
+                    font: '',
+                    text: 'SIDE',
+                    color: '#FF6A00',
+                },
+            }
+            this.dests.push(this.current);
             this.$toast(code + ' added in dests');
         },
-        deleteDest: function(code) {
-            this.$confirm('delete ' + code).then((r) => {
-                if(r) {
-                    delete this.dests[this.current.code];
-                    this.current.code = '';
-                    this.$toast(code + ' deleted from dests');
+        deleteDest: function() {
+            this.$confirm(`Are you sure to delete ${this.current.code} ? (${this.current.index})`).then((r) => {
+                if (r) {
+                    this.dests.splice(this.current.index, 1);
+                    this.$toast(this.current.code + ' deleted from dests');
+                    this.current.code = null;
                 }
             })
         },
-        selectCurrent: function(dest) {
-            this.current = dest
+        selectCurrent: function(index) {
+            this.current = this.dests[index];
+            this.current.index = index;
+            if (this.current.front.color == null) {
+                this.current.front.color = '#FF4400';
+            }
             this.refreshMatrix();
         },
         saveCurrentIntoDests: function() {
-            this.dests[this.current.code] = this.current;
+            this.dests.splice(this.current.index, 1, this.current);
         },
         saveDestsInLocalStorage: function() {
+            this.saveCurrentIntoDests();
             localStorage.data = JSON.stringify(this.dests);
+            this.$toast('Destinations saved in localStorage');
         },
         loadFont: function(font) {
             if(!localStorage[font]) {
                 fetch('./bitmapFonts/'+font+'.json')
                 .then((response) => {
-                    return response.json();
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        this.$toast(`(${font}) error: ${response.status}`);
+                    }
                 })
                 .then((json) => {
                     localStorage[font] = JSON.stringify(json);
@@ -200,6 +291,7 @@ new Vue({
         this.ctx.fillRect(230, 0, 26, 64);
         this.ctx.fillRect(50, 32, 10, 32);
 
-        window.onunload = this.saveDestsInLocalStorage();
+        //window.onunload = this.saveDestsInLocalStorage();
     }
 })
+Vue.config.devtools = true
