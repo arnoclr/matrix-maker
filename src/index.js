d5e0c65cc83814ae546cb5d66961ce8dbbb377c8
Aryqs-Ipsum <mrfeyurikenpro@gmail.com>
2021-02-25 21:49:49 +0100

    assets fingerprint

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import Vue from 'vue';
 import BalmUI from 'balm-ui'; // Official Google Material Components
 import BalmUIPlus from 'balm-ui/dist/balm-ui-plus'; // BalmJS Team Material Components
 
+import './style.css';
+
 Vue.use(BalmUI); // Mandatory
 Vue.use(BalmUIPlus); // Optional
 Vue.config.productionTip = false
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,34 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
     resolve: {
         alias: {
             vue: 'vue/dist/vue.js'
         }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new HtmlWebpackPlugin({
+            filename: '../index.html',
+            template: 'src/template.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ],
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
     }
 }
