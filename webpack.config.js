3cce74e00a812d7627b22ad30cfe6d9d306c2f42
Aryqs-Ipsum <mrfeyurikenpro@gmail.com>
2021-04-29 21:40:04 +0200

    move dev files to tmp dir

--- webpack.config.js
+++ webpack.config.js
@@ -5,55 +5,57 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
-module.exports = {
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.js'
-        }
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css'
-        }),
-        new HtmlWebpackPlugin({
-            filename: '../index.html',
-            template: 'src/template.html'
-        }),
-        new WorkboxPlugin.GenerateSW({
-            clientsClaim: true,
-            skipWaiting: true,
-            swDest: './sw.js'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['@babel/preset-env']
+module.exports = (env, options) => {
+    return {
+        resolve: {
+            alias: {
+                vue: 'vue/dist/vue.js'
+            }
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: '[name].[contenthash].css'
+            }),
+            new HtmlWebpackPlugin({
+                filename: '../index.html',
+                template: 'src/template.html'
+            }),
+            new WorkboxPlugin.GenerateSW({
+                clientsClaim: true,
+                skipWaiting: true,
+                swDest: './sw.js'
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
                     }
                 }
-            }
-        ],
-    },
-    /*optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin({
-                test: /\.css$/i,
-            }),
-        ],
-    },*/
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
+            ],
+        },
+        /*optimization: {
+            minimize: true,
+            minimizer: [
+                new CssMinimizerPlugin({
+                    test: /\.css$/i,
+                }),
+            ],
+        },*/
+        output: {
+            filename: '[name].[contenthash].js',
+            path: path.resolve(__dirname, options.mode === 'production' ? 'dist' : 'tmp')
+        }
     }
 }
